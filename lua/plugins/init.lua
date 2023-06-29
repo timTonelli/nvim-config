cc8b4d63050a7d326dd8bb3da0d9274ce733d429
timTonelli <102175985+timTonelli@users.noreply.github.com>
2023-06-29 16:25:08 -0400

    trim down init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -44,10 +44,11 @@ require('lazy').setup({
       -- Adds a number of user-friendly snippets
       'rafamadriz/friendly-snippets',
     },
+    config = require "plugins.config.nvim-cmp"
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',          opts = {} },
+  { 'folke/which-key.nvim',  opts = {} },
 
   {
     -- Adds git releated signs to the gutter, as well as utilities for managing changes
@@ -112,10 +113,15 @@ require('lazy').setup({
   },
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim',         opts = {} },
+  { 'numToStr/Comment.nvim', opts = {} },
 
   -- Fuzzy Finder (files, lsp, etc)
-  { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = require "plugins.config.telescope"
+  },
 
   -- Fuzzy Finder Algorithm which requires local dependencies to be built.
   -- Only load if `make` is available. Make sure you have the system
@@ -137,6 +143,7 @@ require('lazy').setup({
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
     build = ':TSUpdate',
+    config = require "plugins.config.treesitter"
   },
 
   {
@@ -158,65 +165,7 @@ require('lazy').setup({
       -- Add your own debuggers here
       'leoluz/nvim-dap-go',
     },
-    config = function()
-      local dap = require 'dap'
-      local dapui = require 'dapui'
-      require('mason-nvim-dap').setup {
-        -- Makes a best effort to setup the various debuggers with
-        -- reasonable debug configurations
-        automatic_setup = true,
-        -- You can provide additional configuration to the handlers,
-        -- see mason-nvim-dap README for more information
-        handlers = {},
-        -- You'll need to check that you have the required things installed
-        -- online, please don't ask me how to install them :)
-        ensure_installed = {
-          -- Update this to ensure that you have the debuggers for the langs you want
-          'delve',
-        },
-      }
-
-      -- Basic debugging keymaps, feel free to change to your liking!
-      vim.keymap.set('n', '<F5>', dap.continue)
-      vim.keymap.set('n', '<F1>', dap.step_into)
-      vim.keymap.set('n', '<F2>', dap.step_over)
-      vim.keymap.set('n', '<F3>', dap.step_out)
-      vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint)
-      vim.keymap.set('n', '<leader>B', function()
-        dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-      end)
-
-      -- Dap UI setup
-      -- For more information, see |:help nvim-dap-ui|
-      dapui.setup {
-        -- Set icons to characters that are more likely to work in every terminal.
-        --    Feel free to remove or use ones that you like more! :)
-        --    Don't feel like these are good choices.
-        icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-        controls = {
-          icons = {
-            pause = '⏸',
-            play = '▶',
-            step_into = '⏎',
-            step_over = '⏭',
-            step_out = '⏮',
-            step_back = 'b',
-            run_last = '▶▶',
-            terminate = '⏹',
-            disconnect = "⏏",
-          },
-        },
-      }
-      -- toggle to see last session result. Without this ,you can't see session output in case of unhandled exception.
-      vim.keymap.set("n", "<F7>", dapui.toggle)
-
-      dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-      dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-      dap.listeners.before.event_exited['dapui_config'] = dapui.close
-
-      -- Install golang specific config
-      require('dap-go').setup()
-    end,
+    config = require "plugins.config.nvim-dap"
   },
   {
     "kylechui/nvim-surround",
--- lua/settings.lua
+++ lua/settings.lua
@@ -4,6 +4,16 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 -- vim.g.loaded_netrw = 1
 -- vim.g.loaded_netrwPlugin = 1
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
 
 -------------------------------------- options -----------------------------------------
 
