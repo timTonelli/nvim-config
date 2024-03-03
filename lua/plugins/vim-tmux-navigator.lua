9c79707eec1008f0dea149693e753c373091812b
timTonelli <102175985+timTonelli@users.noreply.github.com>
2024-03-03 12:05:03 -0500

    revert to plugins defaults

--- lua/plugins/vim-tmux-navigator.lua
+++ lua/plugins/vim-tmux-navigator.lua
@@ -8,13 +8,10 @@ return {
         "TmuxNavigatePrevious",
     },
     keys = {
-        { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
-        { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
-        { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
-        { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+        { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+        { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+        { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+        { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
         { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
     },
-    init = function()
-        vim.g.tmux_navigator_preserve_zoom = 1
-    end,
 }
